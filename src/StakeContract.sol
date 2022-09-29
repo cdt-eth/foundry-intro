274c7113c72430881435cfc92a31c519096b1bb9
christianturner <christianturner@hey.com>
2022-09-29 06:35:50 -0400

    add staking function

--- src/StakeContract.sol
+++ src/StakeContract.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract StakeContract{}
+error TransferFailed();
+
+contract StakeContract {
+    mapping(address => mapping(address => uint256)) public s_balances;
+
+    function stake(uint256 amount, address token) external returns (bool) {
+        s_balances[msg.sender][token] += amount;
+
+        bool success = IERC20(token).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+
+        if (!success) revert TransferFailed();
+        return success;
+    }
+}
